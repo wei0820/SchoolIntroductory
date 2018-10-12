7f92e532f7f8fb815d2588308f6ab24e5e5d891d
wei0820 <jheng.wei0919@gmail.com>
2018-10-12 17:25:49 +0800

    add class

---
+++ app/src/main/java/com/jackpan/schoolintroductory/MySharedPrefernces.kt
@@ -0,0 +1,33 @@
+package com.jackpan.schoolintroductory
+
+import android.app.Activity
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.R.id.edit
+import android.content.SharedPreferences
+import android.content.Context.MODE_PRIVATE
+
+
+
+
+
+object MySharedPrefernces {
+    fun setList(context: Context,list :ArrayList<String>){
+        val editor = context.getSharedPreferences("EnvironDataList", MODE_PRIVATE).edit()
+        editor.putInt("EnvironNums", list.size)
+        for (i in list.indices) {
+            editor.putString("item_$i", list[i])
+        }
+        editor.commit()
+    }
+    fun  getList(context: Context):ArrayList<String>{
+        val environmentList = ArrayList<String>()
+        val preferDataList = context.getSharedPreferences("EnvironDataList", MODE_PRIVATE)
+        val environNums = preferDataList.getInt("EnvironNums", 0)
+        for (i in 0 until environNums) {
+            val environItem = preferDataList.getString("item_$i", null)
+            environmentList.add(environItem)
+        }
+        return  environmentList
+    }
+}
