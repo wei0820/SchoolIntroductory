8e41ece7d6048de7cfdc14dd86d922bfed690019
wei0820 <jheng.wei0919@gmail.com>
2018-10-12 17:30:04 +0800

    add RecordActivity

---
+++ app/src/main/java/com/jackpan/schoolintroductory/RecordActivity.kt
@@ -0,0 +1,16 @@
+package com.jackpan.schoolintroductory
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_record.*
+
+class RecordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record)
+    }
+
+}
