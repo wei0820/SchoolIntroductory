88fbcbb103af965c2b821bc6f1b2574dab7652a2
wei0820 <jheng.wei0919@gmail.com>
2018-10-12 22:41:32 +0800

    remove log

--- app/src/main/java/com/jackpan/schoolintroductory/RecordActivity.kt
+++ app/src/main/java/com/jackpan/schoolintroductory/RecordActivity.kt
@@ -1,13 +1,7 @@
 package com.jackpan.schoolintroductory
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-
-import kotlinx.android.synthetic.main.activity_record.*
-import java.text.SimpleDateFormat
-import java.util.*
 
 class RecordActivity : AppCompatActivity() {
 
@@ -15,7 +9,6 @@ class RecordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
         MySharedPrefernces.getList(this).forEach {
-            Log.d("Jack",it)
         }
     }
 
