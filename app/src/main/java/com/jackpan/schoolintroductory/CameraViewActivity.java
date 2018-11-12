1073d1321133361f75074de6a2a7d140213842d8
wei0820 <jheng.wei0919@gmail.com>
2018-11-12 10:16:14 +0800

    no message

--- app/src/main/java/com/jackpan/schoolintroductory/CameraViewActivity.java
+++ app/src/main/java/com/jackpan/schoolintroductory/CameraViewActivity.java
@@ -29,7 +29,6 @@ import java.util.List;
 public class CameraViewActivity extends Activity implements
 		SurfaceHolder.Callback, OnLocationChangedListener, OnAzimuthChangedListener{
 
-
 	private Camera mCamera;
 	private SurfaceHolder mSurfaceHolder;
 	private boolean isCameraviewOn = false;
@@ -46,6 +45,52 @@ public class CameraViewActivity extends Activity implements
 
 	TextView descriptionTextView;
 	ImageView pointerIcon;
+	TextView mTimeText;
+	ImageView mAttackImg ;
+	private SoundPool soundPool,soundPool2;
+	private int alertId,alertId2;
+	int[] mMonsterprimary = new int[]{
+			R.mipmap.monsterprimary_1,
+			R.mipmap.monsterprimary_2,
+			R.mipmap.monsterprimary_3,
+			R.mipmap.monsterprimary_4,
+			R.mipmap.monsterprimary_5,
+			R.mipmap.monsterprimary_6,
+			R.mipmap.monsterprimary_7,
+			R.mipmap.monsterprimary_8,
+			R.mipmap.monsterprimary_9,
+			R.mipmap.monsterprimary10,
+			R.mipmap.monsterprimary_20,
+			R.mipmap.monsterprimary_21,
+			R.mipmap.monsterprimary_22,
+			R.mipmap.monsterprimary_23,
+			R.mipmap.monsterprimary_24,
+			R.mipmap.monsterprimary_25,
+			R.mipmap.monsterprimary_26,
+			R.mipmap.monsterprimary_27,
+			R.mipmap.monsterprimary_28,
+			R.mipmap.monsterprimary_29,
+			R.mipmap.monsterprimary_30};
+
+	int[] mMonsterintermediate = new int[]{
+			R.mipmap.monsterintermediate_1,
+			R.mipmap.monsterintermediate_2,
+			R.mipmap.monsterintermediate_3,
+			R.mipmap.monsterintermediate_4,
+			R.mipmap.monsterintermediate_5,
+			R.mipmap.monsterintermediate_6,
+			R.mipmap.monsterintermediate_7};
+	int[] mMonsterBoss = new int[]{
+			R.mipmap.monsterboss_1,
+			R.mipmap.monsterboss_2};
+	private TextView mHPTextView;
+	long hp ,mhp;
+	long attack; ;
+	long mattck= 1;
+	long time;
+	long m_time ;
+	private ImageView mFireImg;
+	private TextView HpTextView,mFractiText;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -152,18 +197,65 @@ public class CameraViewActivity extends Activity implements
 		mAzimuthReal = azimuthChangedTo;
 		mAzimuthTeoretical = calculateTeoreticalAzimuth();
 
-		pointerIcon = (ImageView) findViewById(R.id.icon);
-
 		double minAngle = calculateAzimuthAccuracy(mAzimuthTeoretical).get(0);
 		double maxAngle = calculateAzimuthAccuracy(mAzimuthTeoretical).get(1);
-
 		if (isBetween(minAngle, maxAngle, mAzimuthReal)) {
 			pointerIcon.setVisibility(View.VISIBLE);
+
 		} else {
 			pointerIcon.setVisibility(View.INVISIBLE);
 		}
 
-		updateDescription();
+		int fire = (int)(Math.random()* 49+1);
+		Log.d(TAG, "onAzimuthChanged: "+fire);
+		int fireran = fire%20;
+		Log.d(TAG, "onAzimuthChanged: "+fireran);
+
+		if (fireran==0){
+			mFireImg.setVisibility(View.VISIBLE);
+			mhp = mhp - mattck;
+			HpTextView.setText("HP:"+mhp);
+			if (mhp==0){
+				HpTextView.setText("HP:0");
+				MySharedPreferncesHelp.saveIsFraction(CameraViewActivity.this,mhp);
+				Toast.makeText(CameraViewActivity.this,"您輸了！！",Toast.LENGTH_SHORT).show();
+				finish();
+			}
+
+		}else {
+			mFireImg.setVisibility(View.GONE);
+
+		}
+		pointerIcon.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mAttackImg.setVisibility(View.VISIBLE);
+				hp = hp - attack;
+				mhp = mhp + attack;
+				mHPTextView.setText("HP:"+hp);
+				soundPool.play(alertId, 1.0F, 1.0F, 0, 0, 1.0F);
+				if (hp==0){
+					mHPTextView.setText("怪物已死亡");
+					Toast.makeText(CameraViewActivity.this,"擊敗怪物！！",Toast.LENGTH_SHORT).show();
+					try {
+						new Thread().sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					MySharedPreferncesHelp.saveIsFraction(CameraViewActivity.this,mhp);
+					finish();
+				}else {
+
+				}
+
+			}
+		});
+		mAttackImg.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mAttackImg.setVisibility(View.INVISIBLE);
+			}
+		});
 	}
 
 	@Override
@@ -190,15 +282,80 @@ public class CameraViewActivity extends Activity implements
 	}
 
 	private void setupLayout() {
-		descriptionTextView = (TextView) findViewById(R.id.cameraTextView);
+		mFireImg = findViewById(R.id.fireimg);
+		mTimeText =findViewById(R.id.time);
+		pointerIcon = (ImageView) findViewById(R.id.icon);
+		HpTextView = findViewById(R.id.mhp);
+		mFractiText = findViewById(R.id.fa);
+		mAttackImg = findViewById(R.id.attack);
+		int mo = (int)(Math.random()*2);
+		int i;
+		switch (mo){
+			case 0:
+				hp = 30;
+				m_time = 30000;
+				attack = 1;
+				i = (int)(Math.random()* mMonsterprimary.length);
+				pointerIcon.setImageResource(mMonsterprimary[i]);
+				break;
+			case 1:
+				hp = 40;
+				m_time = 20000;
+				attack = 5;
+				i = (int)(Math.random()* mMonsterintermediate.length);
+				pointerIcon.setImageResource(mMonsterintermediate[i]);
 
+
+				break;
+			case 2:
+				hp = 50;
+				m_time = 10000;
+				attack = 10;
+				i = (int)(Math.random()* mMonsterBoss.length);
+				pointerIcon.setImageResource(mMonsterBoss[i]);
+				break;
+			default:
+				hp = 30;
+				m_time = 30000;
+
+				i = (int)(Math.random()* mMonsterprimary.length);
+				pointerIcon.setImageResource(mMonsterprimary[i]);
+				break;
+		}
+		mAttackImg.setVisibility(View.INVISIBLE);
+
+		mHPTextView = findViewById(R.id.hp);
+		mhp = MySharedPreferncesHelp.getIsFraction(CameraViewActivity.this);
+		mHPTextView.setText("HP:"+hp);
+		HpTextView.setText("HP:"+mhp);
+		new CountDownTimer(m_time,1000){
+
+			@Override
+			public void onFinish() {
+				mTimeText.setText("時間結束!");
+				if(hp>0){
+					Toast.makeText(CameraViewActivity.this,"沒擊敗怪物 您輸了！！",Toast.LENGTH_SHORT).show();
+					finish();
+				}
+
+			}
+
+			@Override
+			public void onTick(long millisUntilFinished) {
+				mTimeText.setText("00:"+millisUntilFinished/1000);
+				time = millisUntilFinished/1000;
+			}
+
+		}.start();
+		descriptionTextView = (TextView) findViewById(R.id.cameraTextView);
+		descriptionTextView.setVisibility(View.GONE);
 		getWindow().setFormat(PixelFormat.UNKNOWN);
 		SurfaceView surfaceView = (SurfaceView) findViewById(R.id.cameraview);
 		mSurfaceHolder = surfaceView.getHolder();
 		mSurfaceHolder.addCallback(this);
 		mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-	}
 
+	}
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 							   int height) {
