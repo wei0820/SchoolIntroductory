a9be51bbe7ffdcdc20cb80984761f6bc65f9d539
wei0820 <jheng.wei0919@gmail.com>
2018-11-15 17:02:15 +0800

    change method

--- app/src/main/java/com/jackpan/schoolintroductory/MapsActivity.kt
+++ app/src/main/java/com/jackpan/schoolintroductory/MapsActivity.kt
@@ -188,7 +188,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCamera
 //        Toast.makeText(this@MapsActivity,
 //                "距離"+Distance(mLat,mLon,marker.position.latitude,marker.position.longitude)+
 //                        "公尺",Toast.LENGTH_SHORT).show()
-        if(Distance(mLat,mLon,marker.position.latitude,marker.position.longitude)>30000000){
+        if(Distance(mLat,mLon,marker.position.latitude,marker.position.longitude)>300){
             Toast.makeText(this@MapsActivity,"距離太遠了！請再近一點唷！",Toast.LENGTH_SHORT).show()
         }else{
 
